e9f6022b5577cf89545cc2707a8f39a59ac2adaa
Peter Trsko <peter.trsko@gmail.com>
2013-12-06 22:19:59 +0100

    Bootstraping code for executing generated scripts
    Most of the time people want shell scripts to be simple and transparent,
    but there is a domain where this is not one of the main requriements. In
    such cases one might consider creating script that generates another
    script, or other kind of executable, and then executes it. One of such
    use cases is when the result has to be optimized for specific platform
    or Linux distribution.
    
    This commit contains simple function that does allow just that and with
    minimal requirements from the system it is running on. Only thing left
    for the developer of such script is to define generateScript function
    that will create the content of executable that should be executed. Hello
    World example:
    
        #!/bin/bash
    
        set -e
    
        function generateScript()
        {
            cat << EOF
        trap "rm -f -- '\$0'" INT TERM EXIT
        echo 'Hello World!'
        EOF
        }
    
        source "<install-prefix>/snippets/scripts/functions/bootstrap.sh"
    
    Alternative way to define the above code is to inline the content of
    "bootstrap.sh" which should be prefered in cases that the result is
    deployed somewhere.

---
+++ scripts/test/bootstrap-002.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+failure()
+{
+    echo "$testCaseName: Failure."
+    exit 1
+}
+
+success()
+{
+    echo "$testCaseName: Success."
+    exit 0
+}
+
+getFixtureFilePath()
+{
+    local -r startingPoint="$1"; shift
+    local -r testCaseBaseName="$1"; shift
+    local -r fileName="$1"; shift
+
+    result="${testCaseBaseName%-[0-9][0-9][0-9].sh}-fixture/$fileName"
+    result="$startingPoint/$result"
+
+    echo "$result"
+}
+
+getResultFilePath()
+{
+    echo "$(getFixtureFilePath "$@").result"
+}
+
+getInclude()
+{
+    local result="$1"; shift
+    local -r fileName="$1"; shift
+
+    # .../scripts/test/call-stack-001.sh -> .../scripts/test
+
+    # .../scripts/test -> .../scripts/lib/call-trace.sh
+    result="${result%/*}/functions/$fileName"
+
+    echo "$result"
+}
+
+export BOOTSTRAP_INCLUDE="$(getInclude "${0%/*}" 'bootstrap.sh')"
+
+testCase()
+{
+    local -r fixtureName='hello-world'
+
+    local -r startingPoint="${0%/*}"
+    local -r testCaseName="${0##*/}"
+    local -r fixtureFile="$(
+        getFixtureFilePath "$startingPoint" "$testCaseName" "$fixtureName")"
+    local -r resultFile="$(
+        getResultFilePath "$startingPoint" "$testCaseName" "$fixtureName")"
+
+    bash "$fixtureFile" \
+    | cmp --silent "$resultFile" - && success || failure
+}
+
+testCase "$@"
+
+# vim: tabstop=4 shiftwidth=4 expandtab
---
+++ scripts/test/run-tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -a testCases=(
+    'bootstrap-001.sh'
+    'bootstrap-002.sh'
+)
+
+main()
+{
+    local -r parentDir="$(readlink --canonicalize-existing "${0%/*}")"
+    local testCase=''
+
+    for testCase in "${testCases[@]}"; do
+        testCase="$parentDir/$testCase"
+        if [[ -x "$testCase" ]]; then
+            "$testCase" "$@"
+        else
+            echo "${testCase##*/}: File doesn't exist or is not executable."
+        fi
+    done
+}
+
+main "$@"
+
+# vim: tabstop=4 shiftwidth=4 expandtab
