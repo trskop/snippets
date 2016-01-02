7aa3fdd488363cb391a32b4f41d99583449f33c0
Peter Trško <peter.trsko@gmail.com>
2016-01-02 14:14:11 +0100

    Cleanup of Cabal sandbox installation script

--- haskell/diagrams-gtk-example/diagrams-sandbox.sh
+++ haskell/diagrams-gtk-example/diagrams-sandbox.sh
@@ -12,7 +12,5 @@ fi
 cabal sandbox init --sandbox='cabal-sandbox'
 cabal update
 
-# GTK in version 13 and higher is not supported.
-# See https://github.com/haskell/ThreadScope/issues/39
-cabal install gtk2hs-buildtools-0.13.0.5 # --constraint='gtk >= 0.12.1 && <= 0.13'
-cabal install -fcairo -fgtk diagrams-$version # --constraint='gtk >= 0.12.1 && < 0.13'
+cabal install gtk2hs-buildtools-0.13.0.5
+cabal install -fcairo -fgtk diagrams-$version
